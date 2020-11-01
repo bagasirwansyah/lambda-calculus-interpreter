35db8a78c614a5a9c276b363ff1a576b0f1c5d05
Bagas Irwansyah <bagasirwansyah@yahoo.com>
2020-11-01 20:23:47 +0700

    Remove unnecessary symbol when parse error

--- src/Main.hs
+++ src/Main.hs
@@ -17,22 +17,22 @@ strategy "applicative" = applicativeOrderRedex
 
 evaluate :: String -> String -> String
 evaluate expr s = case parseExpr expr of
-                    Left err -> show "Parse Error! ¯\\_(ツ)_/¯"
+                    Left err -> show "Parse Error!"
                     Right e  -> show $ eval e (strategy s)
 
 explainExpr :: String -> String -> String
 explainExpr e1 e2 = case (parseExpr e1, parseExpr e2) of
                     (Right p1, Right p2) -> (explain p1 p2 normalOrderRedex)
-                    otherwise -> "Parse Error! ¯\\_(ツ)_/¯"
+                    otherwise -> "Parse Error!"
 
 alphaEq :: String -> String -> String
 alphaEq e1 e2 = case (parseExpr e1, parseExpr e2) of
                     (Right p1, Right p2) -> if alphaEquivalence p1  p2 then "Alpha Equivalent" else "Not Alpha Equivalent"
-                    otherwise -> "Parse Error! ¯\\_(ツ)_/¯"
+                    otherwise -> "Parse Error!"
 
 debruijnIndex :: String -> String
 debruijnIndex expr = case parseExpr expr of
-                    Left err -> show "Parse Error! ¯\\_(ツ)_/¯"
+                    Left err -> show "Parse Error!"
                     Right e  -> show $ debruijn e
 
 main =  runStateT (runInputT defaultSettings loop) "normal"
